789ee9c51a3c5044ee05a7b56a6c4e9243b3e812
Guyllian Gomez <Guyllian@mac-pro-de-guyllian.home>
2015-12-28 16:41:54 +0100

    Menu finalisé

--- Interface First try/accueilVC.swift
+++ Interface First try/accueilVC.swift
@@ -10,17 +10,23 @@ import UIKit
 
 class accueilVC: UIViewController {
     
+    @IBOutlet weak var navBar: UINavigationBar!
     @IBOutlet weak var menu: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+        // set l'action du bouton menu
         menu.target = self.revealViewController()
         menu.action = Selector("revealToggle:")
         
+        //définie le style du title
+        let navbarFont = UIFont(name: "Century Gothic", size: 25) ?? UIFont.systemFontOfSize(25)
+        
+        navBar.titleTextAttributes = [NSFontAttributeName: navbarFont, NSForegroundColorAttributeName: UIColor(hue: 0.905, saturation: 0.88, brightness: 0.78, alpha: 1)]
+        
+        //permet d'accéder au menu en swipant
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
-        self.navigationController?.navigationBarHidden = true
+
     }
 
     override func didReceiveMemoryWarning() {
